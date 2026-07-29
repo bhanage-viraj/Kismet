147e7f661be844f29eb35f571405a0ebd8ffa7ef
Viraj Bhanage <bhanagevirajvaibhav@gmail.com>
2026-07-29 09:06:22 +0800

    Initial Commit

---
+++ Kismet/KismetApp.swift
@@ -0,0 +1,17 @@
+//
+//  KismetApp.swift
+//  Kismet
+//
+//  Created by Viraj Bhanage on 29/07/26.
+//
+
+import SwiftUI
+
+@main
+struct KismetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
